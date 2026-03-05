4a4439000cdd35ac09b51b68249b552c45004544
nicolas marques <nimarques1207@gmail.com>
2026-03-05 09:35:41 -0300

    arquivos json

--- Aula 07/script.js
+++ Aula 07/script.js
@@ -1,3 +1,5 @@
+const data = require('./data.json')
+
 const pessoas = []
 
 const pessoa1 = {
@@ -35,26 +37,66 @@ pessoas.push(pessoa3)
 pessoas.push(pessoa4)
 pessoas.push(pessoa5)
 
-const filteredPeople = pessoas.filter((p) => p.age >= 20)
-const findLeticia = pessoas.find((p) => p.name == "Leticia" )
+
+// const filteredPeople = pessoas.filter((p) => p.age >= 20)
+// const findLeticia = pessoas.find((p) => p.name == "Leticia" )
 
 
-console.log(filteredPeople)
-console.log(findLeticia)
-console.log(pessoas.indexOf(findLeticia))
+// console.log(filteredPeople)
+// console.log(findLeticia)
+// console.log(pessoas.indexOf(findLeticia))
 
-let container = document.getElementById('container')
+// let container = document.getElementById('container')
 
-function createCard(pessoa){
-    let div = document.createElement('div')
-    let span = document.createElement('span')
-    span.innerHTML = `${pessoa.name} ${pessoa.age}`
-    div.appendChild(span)
+// function createCard(pessoa){
+//     let div = document.createElement('div')
+//     let span = document.createElement('span')
+//     span.innerHTML = `${pessoa?.name} ${pessoa?.age} ${pessoa?.lastname} ${pessoa?.age}`
+//     div.appendChild(span)
 
-    return div
-}
+//     return div
+// }
 
-filteredPeople.forEach(pessoa => {
-    const card = createCard(pessoa)
-    container.appendChild(card)
+// filteredPeople.forEach(pessoa => {
+//     const card = createCard(pessoa)
+//     container.appendChild(card)
+// })
+
+// Método Filter -> Filtrar de um vetor com base em uma condição
+// Método Find -> Encontrar um valor com base em uma condição
+// Método Reduce -> Reduz o vetor para um único valor
+
+const arr = [1,2,3,4,5]
+
+const somaArr = arr.reduce((acc, value) => {
+    return acc + value
+}, 0)
+
+console.log(somaArr)
+
+const ageAvg = pessoas.reduce((acc, pessoa) => {
+    return acc + pessoa.age / pessoas.length
+}, 0)
+
+console.log(ageAvg.toFixed(0))
+
+// Map transforma um vetor de acordo com uma condição
+
+const arr2 = [1,2,3,4,5]
+
+const mappedArr = arr2.map((arr) => {
+    return arr * 2
 })
+
+
+console.log(mappedArr)
+
+console.log(pessoas)
+
+const mappedPeople = pessoas.map((pessoa) => {
+    return { name: pessoa.name, lastname: pessoa.lastname }
+})
+
+console.log(mappedPeople)
+
+console.log(data.vehicles)
